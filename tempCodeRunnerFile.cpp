884653ba9396fb737f03a582f38b8b0236be1b4f
Aakash Kumar Jha <aakash13102005@gmail.com>
2024-04-12 22:51:32 +0530

    Stack using linked list

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,2 +1,9 @@
-
-    cout<<"Popped "<<s1.pop()<<endl;
+  // else{
+    //     node *p=head;
+    //     while (p->next!=top)
+    //     {
+    //         p=p->next;
+    //     }
+    //     delete top; //delete element at top address
+    //     top=p;   //assignin address of p to top  ,p is new top
+    // }
