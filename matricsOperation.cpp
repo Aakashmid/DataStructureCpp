31f8dc5495748696178e411c42a24bdf467dc59f
Aakash Kumar Jha <aakash13102005@gmail.com>
2024-04-28 22:56:55 +0530

    Muliplication operation in matrix

--- matricsOperation.cpp
+++ matricsOperation.cpp
@@ -6,49 +6,38 @@ using namespace std;
 int main()
 {
     cout << "Name - Aakash Kumar Jha \n";
+    int matrix1[3][3] = {{1, 1, 1},
+                         {1, 1, 1},
+                         {1, 1, 1}};
+    int matrix2[3][3] = {{1, 1, 1},
+                         {1, 1, 1},
+                         {1, 1, 1}};
+    // int matrix1[3][3] = {{3, 2, 4},
+    //                      {1, 2, 3},
+    //                      {5, 3, 1}};
+    // int matrix2[3][3] = {{1, 5, 4},
+    //                      {7, 3, 8},
+    //                      {1, 3, 2}};
+    int r1 = 3, c1 = 3;
+    int r2 = 3, c2 = 3;
     int choice;
     do
     {
-        cout << "Operation matrix operation you want to do \n";
-        cout << "1 for addition of two matrices \n";
-        cout << "2 for transpose of a  matrix \n";
-        cout << "3 for multiplication of two matrices \n";
+        cout << "Choose operation you want to do \n";
+        cout << "1 for addition of both matrices \n";
+        cout << "2 for transpose of both  matrix \n";
+        cout << "3 for multiplication of both matrices \n";
+        cout << "Enter your choice :";
         cin >> choice;
         switch (choice)
         {
-        //for adding two matrices
+        // for adding two matrices
         case 1:
         {
-            cout << "First Creating matrices \n";
-            int r1, r2, c1, c2;
-            cout << "Enter number of row and column of matrix first ";
-            cin >> r1 >> c1;
-            int matrix1[r1][c1];
-            cout << "Enter number of row and column of matrix second ";
-            cin >> r2 >> c2;
-            int matrix2[r2][c2];
             if (c1 == c2 && r1 == r2)
             {
-                // Creating first matrix
-                for (int i = 0; i < r1; i++)
-                {
-                    for (int j = 0; j < c1; j++)
-                    {
-                        cout << "Enter value of " << i + 1 << " row and " << c1 << " column : ";
-                        cin >> matrix1[i][j];
-                    }
-                }
-                // Creating second matrix
-                for (int i = 0; i < r2; i++)
-                {
-                    for (int j = 0; j < c2; j++)
-                    {
-                        cout << "Enter value of " << i + 1 << " row and " << c1 << " column : ";
-                        cin >> matrix2[i][j];
-                    }
-                }
-
                 int sumMat[r1][c1]; // result matrix
+                cout << "Sum of both matrix : \n";
                 for (int i = 0; i < r1; i++)
                 {
                     for (int j = 0; j < c1; j++)
@@ -56,7 +45,9 @@ int main()
                         sumMat[i][j] = matrix1[i][j] + matrix2[i][j];
                         cout << sumMat[i][j] << " ";
                     }
+                    cout << "\n";
                 }
+                cout << "\n";
             }
             else
             {
@@ -65,85 +56,74 @@ int main()
             break;
         }
 
-        //For transpose of matrix
+        // For transpose of matrix
         case 2:
         {
-            // creating matrices
-            int r, c;
-            cout << "Enter row and column of matrix :";
-            cin >> r >> c;
-            int Matrix[r][c];
-            for (int i = 0; i < r; i++)
-            {
-                for (int j = 0; j < c; j++)
-                {
-                    cout << "Enter value of row  " << i + 1 << " and  Column " << j + 1<< ": ";
-                    cin >> Matrix[i][j];
-                }
-            }
-
-            // showing created matrix
-            cout << "Created matrix \n{";
-            for (int i = 0; i < r; i++)
-            {
-                cout << "{";
-                for (int j = 0; j < c; j++)
-                {
-                    if (j == c - 1)
-                        cout << Matrix[i][j] << " ";
-                    else
-                        cout << Matrix[i][j] << " ,";
-                }
-                if (i == r - 1)
-                    cout << "}";
-                else
-                {    cout << "},";
-                    cout << "\n";
-                }
-            }
-            cout << "} \n";
-
             // Creating transpose matrix
-            int Transpose[c][r];
-            for (int i = 0; i < r; i++)
+            int Transpose1[c1][r1]; // transpose of first matrix
+            int Transpose2[c2][r2]; // transpose of second matrix
+            for (int i = 0; i < r1; i++)
             {
-                for (int j = 0; j < c; j++)
+                for (int j = 0; j < c1; j++)
                 {
-                    Transpose[j][i] = Matrix[i][j];
+                    Transpose1[j][i] = matrix1[i][j];
+                    // Transpose2[j][i] = matrix2[i][j];
                 }
             }
 
             // showing transpose matrix of created matrix
-            cout << "Transpose of matrix is \n{";
-            for (int i = 0; i < c; i++)
+            cout << "Transpose of matrix1 is -> \n";
+            for (int i = 0; i < c1; i++)
             {
-                cout << "{";
-                for (int j = 0; j < r; j++)
+                for (int j = 0; j < r1; j++)
                 {
-                    if (j == r - 1)
-                        cout << Transpose[i][j] << " ";
-                    else
-                        cout << Transpose[i][j] << " ,";
+                    cout << Transpose1[i][j] << " ";
                 }
-                if (i == c - 1)
-                    cout << "}";
-                else
+                cout << "\n";
+            }
+            cout << "\n";
+            break;
+        }
+
+        // for mutliplication of two matrices
+        case 3:
+        {
+            if (c1 != r2)
+            {
+                cout << "Multiplication of both matrix is not possible !!\n";
+            }
+            else
+            {
+                int res[r1][c2]={0}; // resultant matrix
+                // multiplying  matrices
+
+                
+                // for (int i = 0; i < r1; ++i)
+                // {
+                //     for (int j = 0; j < c2; ++j)
+                //     {
+                //         for (int k = 0; k < c2; ++k)
+                //         {
+                //             res[i][j]=res[i][j]+(matrix1[i][k]*matrix2[k][j]);
+                //         }
+                //     }
+                // }
+
+                cout << "Resultant matrix : -> \n";
+                for (int i = 0; i < r1; i++)
                 {
-                    cout << "},";
+                    for (int j = 0; j < c2; j++)
+                    {
+                        cout << res[i][j] << " ";
+                    }
                     cout << "\n";
                 }
+                cout << "\n";
             }
-            cout << "} \n";
             break;
         }
-
-        //for mutliplication of two matrices
-        case 3:{
-
         }
-        default:
-            break;
-        }
+        cout << endl;
     } while (1 >= choice <= 3);
     return 0;
 }
